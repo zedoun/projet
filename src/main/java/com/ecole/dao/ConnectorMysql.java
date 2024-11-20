8841a02680355bd8c0cad4aa18171a6eefcff504
zedoun <zedounalghanem@gmail.com>
2024-11-20 16:25:13 +0100

    update  classe de cours

--- src/main/java/com/ecole/dao/ConnectorMysql.java
+++ src/main/java/com/ecole/dao/ConnectorMysql.java
@@ -11,7 +11,7 @@ public class ConnectorMysql {
 	private Connection con = null;
 
 	private String url = "jdbc:mysql://localhost/gestion_school";
-	private String user = "changez le nom de user";
+	private String user = "root";
 	private String password = "définez le mot de pass";
 
 	public ConnectorMysql() {
