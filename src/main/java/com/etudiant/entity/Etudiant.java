19c67a906d7c2004fe55e5913e7c5478f0d2892d
guenebem syriaque <guenebem@gmail.com>
2024-11-22 12:13:48 +0100

    jeansy est un sauvage

---
+++ src/main/java/com/etudiant/entity/Etudiant.java
@@ -0,0 +1,69 @@
+package com.etudiant.entity;
+
+public class Etudiant {
+
+	private int id_etudiant;
+	 private String nom;
+	    private String prenom;
+	    private int age;
+	    private String matricule;
+	    private double moyenne;
+		public Etudiant() {
+			super();
+		}
+		public Etudiant(int id_etudiant, String nom, String prenom, int age, String matricule, double moyenne) {
+			super();
+			this.id_etudiant = id_etudiant;
+			this.nom = nom;
+			this.prenom = prenom;
+			this.age = age;
+			this.matricule = matricule;
+			this.moyenne = moyenne;
+		}
+		public int getId_etudiant() {
+			return id_etudiant;
+		}
+		public void setId_etudiant(int id_etudiant) {
+			this.id_etudiant = id_etudiant;
+		}
+		public String getNom() {
+			return nom;
+		}
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		public String getPrenom() {
+			return prenom;
+		}
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getMatricule() {
+			return matricule;
+		}
+		public void setMatricule(String matricule) {
+			this.matricule = matricule;
+		}
+		public double getMoyenne() {
+			return moyenne;
+		}
+		public void setMoyenne(double moyenne) {
+			this.moyenne = moyenne;
+		}
+		@Override
+		public String toString() {
+			return "Etudiant [id_etudiant=" + id_etudiant + ", nom=" + nom + ", prenom=" + prenom + ", age=" + age
+					+ ", matricule=" + matricule + ", moyenne=" + moyenne + ", getId_etudiant()=" + getId_etudiant()
+					+ ", getNom()=" + getNom() + ", getPrenom()=" + getPrenom() + ", getAge()=" + getAge()
+					+ ", getMatricule()=" + getMatricule() + ", getMoyenne()=" + getMoyenne() + ", getClass()="
+					+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+		}
+	    
+	    
+}
---
+++ src/main/java/com/etudiant/entity/Test.java
@@ -0,0 +1,12 @@
+package com.etudiant.entity;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Etudiant e1 = new Etudiant();
+		System.out.println(e1);
+
+	}
+
+}
